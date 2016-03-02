679d449cb6470a79d61d0e3dacfbd9f25f0ceef7
Dan Farrelly <daniel.j.farrelly@gmail.com>
2016-03-02 00:02:35 -0500

    Add upload task for hosted, versioned icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,15 @@
+var pkg = require('./package.json');
+
+var version = pkg.version;
+
 module.exports = function(grunt){
   grunt.initConfig({
+
     webfont: {
       icons: {
         src: 'src/icons/*.svg',
         dest: 'dist/webfonts',
-        destCss: 'dist/less',
+        destCss: 'dist',
         options: {
           font: 'buffer-icons',
           fontFilename: 'buffer-icons',
@@ -13,14 +18,40 @@ module.exports = function(grunt){
             baseClass: 'bi',
             classPrefix: 'bi-',
             mixinPrefix: 'bi_',
-          }, 
-          stylesheet: 'less'
+          }
         }
       }
+    },
+
+    aws_s3: {
+      options: {
+        // Must include AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables
+        region: 'us-east-1',
+        bucket: 'buffer-icons',
+        params: {
+          CacheControl: 'public, max-age=31520626'
+        },
+        overwrite: false
+      },
+      production: {
+        files: [
+          {
+            expand: true,
+            cwd: 'dist',
+            src: ['**'],
+            dest: version + '/'
+          }
+        ]
+      }
+      // IDEA: Add future 'staging' for branch builds during dev
     }
+
   });
 
   grunt.loadNpmTasks('grunt-webfont');
+  grunt.loadNpmTasks('grunt-aws-s3');
+
+  grunt.registerTask('upload', ['aws_s3']);
 
   grunt.registerTask('default', ['webfont']);
 };
